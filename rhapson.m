8f26218b554f0ab02650b13c61ae41c15ca3cc39
Serena Lee <100086694+slee4689@users.noreply.github.com>
2023-11-21 15:37:04 -0500

    Add files via upload

--- rhapson.m
+++ rhapson.m
@@ -1,4 +1,4 @@
-function [output] = rhapson(Ri, Ro, lambda, Pi)
+function [output] = rhapson(Ri, Ro, lambda, Pi, materialParameters)
 
 % calculate the current inner radius (using the reference as a guess)
 ri = sqrt(Ro.^2-1./lambda*(Ro^2-Ri^2)); 
@@ -20,7 +20,7 @@ for index1 = 0:n-1
     F_vector = [(1/lambda) * (R1/r1), r1/R1, lambda];
     F1 = diag(F_vector); % define the the deformation gradient tensor (use the diag function)
 
-    sigma_extra1 = Constitutive_model(F1); % calculate sigma_extra using the constitutive model
+    sigma_extra1 = Constitutive_model(F1, materialParameters); % calculate sigma_extra using the constitutive model
     pressure1 = ((1/r1) * (sigma_extra1(2,2) - sigma_extra1(1, 1))) * (R1/(lambda * r1)); % evaluate the function inside the pressure integral, at R1
     force1 = (2*sigma_extra1(3, 3) + sigma_extra1(2, 2) + sigma_extra1(1, 1)) * (R1/(lambda * r1)) * r1; % evaluates the function inside the force integral, at R1
 
@@ -29,7 +29,7 @@ for index1 = 0:n-1
 
     F_vector = [(1/lambda) * (R2/r2), r2/R2, lambda];
     F2 = diag(F_vector); % define the the deformation gradient tensor (use the diag function)
-    sigma_extra2 = Constitutive_model(F2); % calculate sigma_extra using the constitutive model
+    sigma_extra2 = Constitutive_model(F2, materialParameters); % calculate sigma_extra using the constitutive model
     pressure2 = ((1/r2) * (sigma_extra2(2,2) - sigma_extra2(1, 1))) * (R2/(lambda * r2)); % evaluate the function inside the integral, at R2
     force2 = (2*sigma_extra2(3, 3) + sigma_extra2(2, 2) + sigma_extra2(1, 1)) * (R2/(lambda * r2)) * r2; % evaluates the function inside the force integral, at R1
 
