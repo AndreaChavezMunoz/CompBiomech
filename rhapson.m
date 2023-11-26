ca7fef8688a8b4f3db36ea865bd5410fb909721f
Andrea Chavez Munoz <chavezmunoz.a@northeastern.edu>
2023-11-25 21:01:33 -0500

    rahpson implemented to main script

--- rhapson.m
+++ rhapson.m
@@ -1,12 +1,13 @@
-function [x] = raphson(x0, g)
+function [x] = rhapson(x0, g)
 %{
 x0 is the initial guess
-g is the equilibrium function to be minimized
+g is the equilibrium function to be minimized that returns the value to be
+minimized in the position (1,1) of result
 %}
 
 % Iteration Constraints
-tol = 0.001;    % tolerance criteria to stop scheme
-maxiter = 10000;  % maximum number of iterations allowed before stopping
+tol = 0.01;    % tolerance criteria to stop scheme
+maxiter = 100;  % maximum number of iterations allowed before stopping
 
 % Newton Raphson Iteration 
 % THIS NEEDS TO BE UPDATED, IDK IF THIS IS THE RIGHT WAY OF TESTING 
@@ -20,10 +21,10 @@ else
     % first perform Newton Raphson for the initial guess 
     
     % evaluate the function, f, with the initial guess, x0
-    f = g(x0); % call: equilibrium_r_or_loaded_tutorial
+    f = g(x0); % call: equilibrium
     
     iter = 0; % initial iteration
-    h = 0.1; % step size to perform the derivative (forward difference)    
+    h = 0.01; % step size to perform the derivative (forward difference)    
     
     xinc = x0; % initial guess of the root
     xinc = xinc + h; % current root + derivative step
@@ -36,7 +37,8 @@ else
     % defined tolerance) or you have reached the maximum number of
     % iterations
     
-    while iter < maxiter  % iterate until one of the criteras are met
+    while iter < maxiter || x < tol % iterate until one of the criteras are met
+        
         
         iter = iter+1; % current iteration number        
         f = g(x); % evaluate f at x
@@ -44,6 +46,7 @@ else
         xinc = xinc + h; % current root + derivative step
         f_prime = (g(xinc)-f)/h; % compute the derivative of f, between x and xinc
         u = f/f_prime; % divide f by its derivative, f/f'
+        
         x = x-u; % update guess for ro: X_n = X_(n-1) - f/f' (Newton-Raphson Method)
         
     end
